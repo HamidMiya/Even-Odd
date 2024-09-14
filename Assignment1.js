b985f4e41e84edf9f32229fc525751e20285b374
Muhammad Hamid Miya <160655853+HamidMiya@users.noreply.github.com>
2024-09-14 11:59:45 +0500

    Updated
    In this Code i have done 2 TOY program 1 is EVEN or ODD program 2 is To check percentage

---
+++ Assignment1.js
@@ -0,0 +1,41 @@
+console.log("Welcome to Learn JavaScript");
+
+console.log("Even or Odd Program");
+
+let a = prompt("Enter The number to check Even / Odd");
+if(a%2==0){
+    console.log("Even Number");}
+    else{
+        console.log("Odd Number");
+
+    }
+
+
+
+
+let persentage=prompt("Enter your Persentage");
+
+
+if(persentage >= 80 && persentage <=100){
+    console.log("A1 Grade")
+}
+else if(persentage >=101 || persentage <=0){
+    console.log("Invalid Input");
+}
+else if(persentage >=70 && persentage <= 79){
+    console.log("B Grade");
+}
+else if(persentage >=60 && persentage <= 69){
+    console.log("C Grade");
+}else if(persentage >=50 && persentage <= 59){
+    console.log("D Grade");
+}else{
+        console.log("Fail")
+}
+
+
+
+
+
+
+
